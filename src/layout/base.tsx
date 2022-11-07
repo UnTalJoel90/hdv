8ba021d1af60e3a4294f277aba5fa5aed052bf15
JoelRobinson90 <j.robinson.alvarez@gmail.com>
2022-11-07 14:26:56 -0500

    mini change

---
+++ src/layout/base.tsx
@@ -0,0 +1,47 @@
+// import { navigate } from 'gatsby';
+import React, { FC, useEffect } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import Footer from '../components/Footer';
+// import NaBar from '../components/NavBar';
+// import { useNavBarMenuLinks } from '../hooks/useNavBarMenuLinks';
+import { GlobalStyle, theme } from '../theme';
+import type { BaseProps /*, NavBarMenuLinksProps*/ } from '../types/interface';
+
+export const Base: FC<BaseProps> = ({ children }) => {
+  // const menuList: NavBarMenuLinksProps[] = useNavBarMenuLinks();
+  // const goNavigation = (url: string) => navigate(url);
+
+  const disabledRightClick = (e: any) => {
+    e.preventDefault();
+  };
+
+  const dom = typeof window != 'undefined';
+
+  useEffect(() => {
+    if (dom) {
+      window.addEventListener('mousemove', () => {
+        // eslint-disable-next-line no-debugger
+        debugger;
+      });
+      window.addEventListener('contextmenu', disabledRightClick);
+
+      return () => {
+        window.removeEventListener('contextmenu', disabledRightClick);
+        window.removeEventListener('mousemove', () => {
+          // eslint-disable-next-line no-debugger
+          debugger;
+        });
+      };
+    }
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      {/* <NaBar menuList={menuList} goNavigation={goNavigation} /> */}
+      {children}
+      <Footer />
+    </ThemeProvider>
+  );
+};
