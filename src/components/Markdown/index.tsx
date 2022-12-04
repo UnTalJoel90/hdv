52eb5ee4dbce92fdf97284f260092a226188e615
JoelRobinson90 <j.robinson.alvarez@gmail.com>
2022-12-04 18:28:00 -0500

    fix render text

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -1,15 +1,18 @@
 import styled, { css } from 'styled-components';
 
-import { MainParagraph } from '../Typography';
-
-export const Markdown = styled(MainParagraph)`
-  ${({ theme: { colors } }) => css`
+export const Markdown = styled.div`
+  ${({ theme: { colors, fontSizes, breakpoints } }) => css`
     a {
       color: ${colors.greenHeaven};
       font-weight: 700;
     }
     p {
       margin-bottom: 10px;
+      font-size: ${fontSizes[2]};
+
+      ${breakpoints.md} {
+        font-size: ${fontSizes[3]};
+      }
 
       &:last-child {
         margin-bottom: 0;
--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -24,7 +24,7 @@ const ProjectTemplates: FC<ProjectTemplatesProps> = ({ data }) => {
         <Space space="30px" />
         <Title>{title}</Title>
         <Space space="20px" />
-        <Markdown dangerouslySetInnerHTML={{ __html: `<div>${descriptionHtml}</div>` }} />
+        <Markdown dangerouslySetInnerHTML={{ __html: descriptionHtml }} />
         <Space space="20px" />
         <SubTitle>QR</SubTitle>
         <Space space="10px" />
